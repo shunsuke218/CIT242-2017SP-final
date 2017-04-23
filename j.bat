5e39c7cb655049c8f385f3a8e97d8baedc6d799f
shunsuke218 <shunsuke218@gmail.com>
2017-04-23 10:48:18 -0400

    Update j.bat
    Update %PATH% within this batch job.

--- j.bat
+++ j.bat
@@ -1,3 +1,4 @@
 cls
+PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\system32\WBEM;c:\Program Files\Microsoft SQL Server\90\Tools\binn\;C:\Program Files\Microsoft SQL Server\80\Tools\Binn\;c:\program files\java\jdk1.8.0_91\bin
 javac %1.java
 java %1
